52630854e22d2e83c5235bc1560bec13c069e440
Samir <samy.issaad@outlook.com>
2020-01-17 16:12:17 +0100

    Fin

--- JeuDeCartes.cc
+++ JeuDeCartes.cc
@@ -5,35 +5,18 @@ int JeuDeCartes::tailleDeck = 6;  //6 * 1 ; 3 * 2 ; 1 * 3 ; 2*+2 ; 2*-2
 
 JeuDeCartes::JeuDeCartes(){
 	//initialiser le deck de jeu
-	/*e1 = CarteEau(1,"Extincteur","Réduit le taux de fumée de 1.");
-	e2 = CarteEau(2,"Lance_eau","Réduit le taux de fumée de 2.");
-	e3 = CarteEau(3,"Bombardier_eau","Réduit le taux de fumée de 3.");
-	f1 = CarteFeu(1,"Grenade","Augmente le taux de fumée de 1.");
-	f2 = CarteFeu(2,"Cocktail_molotov","Augmente le taux de fumée de 2.");
-	f3 = CarteFeu(3,"Lance_flammes","Augmente le taux de fumée de 3.");
-
-	c1 = CarteFeu(2, "Compagnon_Boule", "Augmente les dégats d'un personnage Feu de +2.");
-	c2 = CarteEau(2, "Compagnon_aquatique", "Augmente les dégats d'un personnage Eau de +2.");*/
-	//deck[new CarteFeu(1,"Extincteur","Augmente les dégats de tous les personnages Eau de 1.","Soin")] = 6;
-	/*deck[new CarteFeu(1,"Grenade","Augmente les dégats de tous les personnages Feu de 1.")] = 6;
-	deck[new CarteEau(2,"Lance eau","Augmente les dégats de tous les personnages Eau de 3.")] = 3;
-	deck[new CarteFeu(2,"Cocktail_molotov","Augmente les dégats de tous les personnages Feu de 2.")] = 3;
-	deck[new CarteEau(3,"Bombardier à eau","Augmente les dégats de tous les personnages Eau de 3.")] = 1;
-	deck[new CarteFeu(3,"Lance_flammes","Augmente les dégats de tous les personnages Feu de 3.")] = 1;
-	deck[new CarteFum(2, "Compagnon_Boule", "Augmente le taux de Fumée de 2.")] = 2;
-	deck[new CarteFum(-2, "Compagnon_aquatique", "Reduit le taux de Fumée de 2.")] = 2;*/
-	
 	deck[new Tsunami()] = 6;
 	deck[new Volcano()] = 6;
 	deck[new Boisson()] = 3;
 	deck[new Brasier()] = 3;
-	deck[new CarteFum(2, "Compagnon_Boule", "Augmente le taux de Fumée de 2.")] = 2;
-	deck[new CarteFum(-2, "Compagnon_aquatique", "Reduit le taux de Fumée de 2.")] = 2;
+	deck[new CarteFum(2, "Molotov", "Augmente le taux de Fumée de 2.")] = 2;
+	deck[new CarteFum(-2, "Extincteur", "Reduit le taux de Fumée de 2.")] = 2;
 }
 
 Carte* JeuDeCartes::piocher(){
 	if(deck.empty()){
-		std::cout << "=== Pioche vide : le joueur ayant le moins de PV perd la partie ===" << std::endl;
+		std::cout << "=== Pioche vide : le joueur gagne la partie ===" << std::endl;
+		exit(0);
 	}
 	srand(time(NULL));
 	int random = rand() % tailleDeck , i = 0;
--- main.cc
+++ main.cc
@@ -1,57 +1,26 @@
 #include <string>
-/*#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>*/
-//A SUPPR APRES
 #include "ChevalierEau.hh"
 #include "Personnage.hh"
 #include "ChevalierFeu.hh"
 #include "JeuDeCartes.hh"
 #include "Carte.hh"
-#include "CarteEau.hh"
-//#include "CarteFeu.hh"
 
 using namespace std;
 
 int main(){
-	/*SDL_Init(SDL_INIT_VIDEO);
-	SDL_Event event;
-
-	    SDL_Window * window = SDL_CreateWindow("SDL2 SH13",
-	        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 768, 0);
-
-	    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-	    SDL_Surface *gobutton;
-	 	gobutton = IMG_Load("gobutton.png");
-	 	SDL_Texture *texture_gobutton;
-	 	texture_gobutton = SDL_CreateTextureFromSurface(renderer, gobutton);
-	 	SDL_Rect dstrect = { 500, 350, 200, 150 };
-	 	SDL_RenderCopy(renderer, texture_gobutton, NULL, &dstrect);
-	while(event.type != SDL_QUIT){
-	 	SDL_PollEvent(&event);
- 	}*/
-    //SDL_Quit();
-   /* JeuDeCartes j;
-    Carte* c = j.piocher();
-    int a;
-    while(1){
-   		cin >> a; 
-    	c = j.piocher();
-    }*/
-
     JeuDeCartes j;
-    ChevalierEau jean(90, 10, "Damso", 1);
-    jean.jouer("");
+    ChevalierEau chev(90, 7, "Afif", 1);
+    chev.jouer(""); //initialisation des ennemis
 
     string cible;
     int i = 1;
     while(1){	
         cout << "=================== TOUR : " << i << " ==================="<< endl << endl ;
     	Carte* carte = j.piocher();
-    	jean.prendreCarte(*carte);
+    	chev.prendreCarte(*carte);
     	cout << "Entrez le nom de votre cible : ";
     	cin >> cible;
-	    jean.jouer(cible);
+	    chev.jouer(cible);
         i++;
         cout << endl << endl << endl << endl << endl;
     }
